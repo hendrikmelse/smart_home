a1b0a679c774d2e61468c0be099dbeee8f4151ad
Hendrik Melse <hendrik.melse@wsu.edu>
2021-08-24 22:07:15 -0700

    Added some commands to server manager

--- server_manager.py
+++ server_manager.py
@@ -14,9 +14,62 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((args.ip, args.port))
 
+        while True:
+            print()
+            action = menu(title="Choose an action", options=[
+                'List registered devices',
+                'Register self as "server_manager"',
+                'Unregister a device',
+                'Get info about a device',
+            ])
+            print()
+            if action == 0:  # List devices
+                print('Registered devices:')
+                devices = get_devices(sock)
+                if len(devices) == 0:
+                    print('No registered devices')
+                else:
+                    for device in get_devices(sock):
+                        print(device)
+            elif action == 1:  # Register self
+                sock.sendall(format_server_message(
+                    command='register',
+                    name='server_manager',
+                    description='A tool to access the server manually',
+                ))
+                response = json.loads(sock.recv(1024).decode())
+                if response['success']:
+                    print('success')
+                else:
+                    print('Something went wrong')
+            elif action == 2:  # Unregister a device
+                ...
+            elif action == 3:  # Get info about a device
+                ...
+
+def menu(title=None, options=None):
+    """Display a simple console menu. Return the index of the option"""
+
+    if title is not None:
+        print(title)
+
+    if options is None:
+        print('No options to choose from')
+        return None
+    
+    for i in range(len(options)):
+        print(f'{i + 1:4} - {options[i]}')
+
+    choice = 0
+    while choice < 1 or choice > len(options):
+        print(f'Choose an option ({1}-{len(options)}): ', end=None)
+        choice = int(input())
+
+    return choice - 1
 
 def get_devices(sock):
-    response = sock.send(format_server_message('get_devices').encode())
+    sock.sendall(format_server_message('get_devices'))
+    response = json.loads(sock.recv(1024).decode())
     return response.get('response', None)
 
 def format_server_message(command, **kwargs):
@@ -32,7 +85,7 @@ def format_server_message(command, **kwargs):
             'command': command,
             **kwargs,
         },
-    })
+    }).encode()
 
 if __name__ == '__main__':
     main()
